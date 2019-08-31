5c157aec4c8c2b8ff92eb71a9e2afbc70f3736b3
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-08-30 19:10:52 -0700

    PLayer Movement

---
+++ Assets/Mitchell/Scripts/PlayerHandler1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerHandler1 : MonoBehaviour
+{
+    public float maxHealth, maxMana, maxStamina;
+    public float currentHealth, currentMana, currentStamina;
+
+    private UIManager _uiManager;
+
+    void Awake()
+    {
+        _uiManager = GetComponent<UIManager>();
+    }
+
+    void Start()
+    {
+        maxHealth = 100;
+        maxMana = 100;
+        maxStamina = 100;
+        currentHealth = 100;
+        currentMana = 100;
+        currentStamina = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Mitchell/Scripts/UIManager1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mitchell
+{
+    public class UIManager1 : MonoBehaviour
+    {
+        #region Singleton
+        public static UIManager1 Instance = null;
+        private void Awake()
+        {
+            Instance = this;
+        }
+        #endregion
+
+        public Text scoreText;
+
+        public Slider healthBar;
+        public Slider manaBar;
+        public Slider staminaBar;
+
+        public void UpdateScore(int score)
+        {
+            // Change the score Text to updated value
+            scoreText.text = "Diamonds: " + score.ToString();
+        }
+    }
+}
