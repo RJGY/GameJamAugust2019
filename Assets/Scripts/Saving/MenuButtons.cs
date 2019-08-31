db326145ea54f21467b725ed7b14f2f66b208edf
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-08-30 17:03:06 -0700

    Save stuff

---
+++ Assets/Scripts/Saving/MenuButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void ChangeScene(int sceneIndex)
+    {
+        //changes the scene to the second and starts the game
+        SceneManager.LoadScene(sceneIndex);
+
+    }
+    //gets you out of the program
+    public void NextLevel()
+    {
+        GetComponent<MenuButtons>().ChangeScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
