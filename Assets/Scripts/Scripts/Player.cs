9fba7778cfceb0d7013f2e9adf10fd55a4bb15c8
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-08-31 22:00:04 +1000

    Animation

--- Assets/Scripts/Scripts/Player.cs
+++ Assets/Scripts/Scripts/Player.cs
@@ -56,6 +56,7 @@ public class Player : MonoBehaviour
                 controller.Jump(jumpHeight);
 
             }
+     
             controller.Climb(vertical * climbSpeed);
         
             
