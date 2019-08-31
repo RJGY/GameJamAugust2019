44a7accca428489b839d5442cff1bf02e6d264ac
RJGY <rjgy2yu@gmail.com>
2019-09-01 02:11:55 +1000

    Added enemy death

---
+++ Assets/Scripts/Enemy/EnemyDeath.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+namespace Reese
+{
+
+
+    public class EnemyDeath : MonoBehaviour
+    {
+        public bool enemyDead = false;
+        public Animator anim;
+        private Guard guard;
+        private Runner runner;
+        private Turret turret;
+        private TriggerEvent triggerEvent;
+        // Start is called before the first frame update
+        void Start()
+        {
+            triggerEvent = GetComponent<TriggerEvent>();
+            guard = GetComponent<Guard>();
+            runner = GetComponent<Runner>();
+            turret = GetComponent<Turret>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (enemyDead)
+            {
+                OnDeath();
+            }
+        }
+
+        void OnDeath()
+        {
+
+            // Play respective enemy death animation.
+            // if (guard)
+            // anim.DeathAnimation
+
+            Destroy(triggerEvent);
+
+            if (guard != null)
+            {
+                Destroy(guard);
+            }
+
+            else if (runner != null)
+            {
+                Destroy(runner);
+            }
+
+            else if (turret != null)
+            {
+                Destroy(turret);
+            }
+
+            
+            // After death animation, destroy the whole gameoject
+            Invoke("DestroyAll", 3f);
+
+        }
+
+        void DestroyAll()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
+
