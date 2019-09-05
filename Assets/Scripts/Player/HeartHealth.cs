0b52c394da25228a6b9b622d3bad0b730f9a0deb
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-09-05 12:31:28 +1000

    added reese and lucy things

---
+++ Assets/Scripts/Player/HeartHealth.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HeartHealth : MonoBehaviour
+{
+    private PlayerHandler _playerHandler;
+
+    [Header("Player Statistics")]
+    [SerializeField]
+    private float currentHealth, maxHealth;
+
+    [Header("Heart Slots")]
+    public Image[] heartSlots;
+    public Sprite[] heartSprite;
+    private float healthPerSection;
+
+    private void Start()
+    {
+        _playerHandler = GetComponent<PlayerHandler>();
+        // Calculate the health points per heart section.
+
+        maxHealth = _playerHandler.maxHealth; // Grab the amount of max Health from the player handler.
+        healthPerSection = maxHealth / heartSlots.Length;
+    }
+
+    private void Update()
+    {
+        if (currentHealth != _playerHandler.curHealth)
+        {
+            currentHealth = _playerHandler.curHealth;
+            UpdateHeart();
+        }
+    }
+
+    void UpdateHeart()
+    {
+        // Index variable starting at 0 for our slot check.
+        // For all the hearts we have:
+        for (int i = 0; i < heartSlots.Length; i++)
+        {
+            // If our health is greater or equal to the slot amount.
+            if (currentHealth >= (healthPerSection) + healthPerSection * i)
+            {
+                heartSlots[i].sprite = heartSprite[0]; // Shows player with full hearts
+            }
+
+            else
+            {
+                // You are supposed to be dead here
+
+                heartSlots[i].sprite = heartSprite[2]; // Shows player with no hearts for that section.
+            }
+        }
+    }
+}
+
