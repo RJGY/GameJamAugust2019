64cab6e7961ccb744b6bc3f42f47a29d57e01ec8
Reese <rjgy2yu@gmail.com>
2019-09-12 01:42:21 +1000

    Added Coin script
    added coin script

---
+++ Assets/Scripts/Items/Coin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private GameManager _gameManager;
+    private float _rotationSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y + Time.deltaTime * _rotationSpeed, transform.rotation.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _gameManager.score++;
+            gameObject.SetActive(false);
+        }
+    }
+}
