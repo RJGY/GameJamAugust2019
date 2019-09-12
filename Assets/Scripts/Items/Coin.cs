7214a213c0610e8764051990d9194113a1a42222
RJGY <rjgy2yu@gmail.com>
2019-09-12 10:46:04 +1000

    Coin Script and Prefab
     - Cmade the coin script work
     - Made the coin prefab

--- Assets/Scripts/Items/Coin.cs
+++ Assets/Scripts/Items/Coin.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     private GameManager _gameManager;
-    private float _rotationSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        _gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y + Time.deltaTime * _rotationSpeed, transform.rotation.z);
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
